331b62055d985e59e39d0bc6ae7091b02440766a
Konstantin <2447785@gmail.com>
2016-10-13 10:27:34 +0300

    Init user model

--- tp_databases/settings.py
+++ tp_databases/settings.py
@@ -11,12 +11,8 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
 
     'user',
 ]
@@ -26,8 +22,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """
+    Main user model
+    """
+    username = models.CharField(max_length=40, db_index=True)
+    email = models.CharField(max_length=100, unique=True, db_index=True)
+    about = models.TextField()
+    name = models.CharField(max_length=40)
+
+    isAnonymous = models.BooleanField(blank=True, default=False)
